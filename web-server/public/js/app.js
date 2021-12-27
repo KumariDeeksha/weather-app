57d4be53a4149025b698cbcbba09956b6c319c76
KumariDeeksha <deeku2117@gmail.com>
2021-12-27 14:48:52 +0530

    some updates

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,21 @@
 console.log("hello");
+
+// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
+//     response.json().then((data)=> {
+//       console.log(data)
+//     })
+// }
+// )
+fetch('http://localhost:805/weather?address=boston').then((response)=>{
+    response.json().then((data) =>{
+        if(data.error){
+            console.log(data.error)
+        }
+        else{
+            console.log(data.location)
+            console.log(data.forecast)
+
+        }
+    })
+})
+
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express');
-const hbs= require('hbs')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast =require('./utils/forecast')
 
 const app = express()
 const publicDirectoryPath=path.join(__dirname, '../public');
@@ -40,25 +42,38 @@ app.get('/weather', (req, res) => {
         })
     }
 })
+app.get('/products', (req,res)=>{
+    if(!req.query.search)
+    {
+       res.send({
+           error:'provide a search term'
+       })
+    }
+    console.log(req.query.search)
+    return res.send({
+        product:[]
+    })
+}
+)
 
-//     geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-//         if (error) {
-//             return res.send({ error })
-//         }
+    geocode(req.query.address, (error, { latitude, longitude, location } ) => {
+        if (error) {
+            return res.send({ error })
+        }
 
-//         forecast(latitude, longitude, (error, forecastData) => {
-//             if (error) {
-//                 return res.send({ error })
-//             }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
 
-//             res.send({
-//                 forecast: forecastData,
-//                 location,
-//                 address: req.query.address
-//             })
-//         })
-//     })
-// })
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+
 
 app.get('*', (req, res) => {
     res.render('404', {
